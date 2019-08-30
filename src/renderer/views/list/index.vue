df8042f8f6fd8ccb2d68c8c20645b1a45db4e4fb
kenshin627 <87zhaoxiaobo@163.com>
2019-08-30 21:37:27 +0800

    first commit

---
+++ src/renderer/views/list/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app-container">
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
+      <el-table-column align="center" label='类别' width="95">
+        <template slot-scope="scope">
+          {{scope.$index}}
+        </template>
+      </el-table-column>
+      <el-table-column label="描述">
+        <template slot-scope="scope">
+          {{scope.row.title}}
+        </template>
+      </el-table-column>
+      <el-table-column label="金额" width="110" align="center">
+        <template slot-scope="scope">
+          <span>{{scope.row.author}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="日期" width="110" align="center">
+        <template slot-scope="scope">
+          {{scope.row.pageviews}}
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+
+export default {
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
